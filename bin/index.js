9ac2c7aad1f9070e536a84c7275a61107f033f77
GONGXIAOBAI\gong <517441540@qq.com>
2021-06-13 22:36:06 +0800

    feat: 简单实现

---
+++ bin/index.js
@@ -0,0 +1,10 @@
+#!  /usr/bin/env node
+
+const path = require('path')
+
+//   拿配置文件
+const config = require(path.resolve('webpack.config.js'))
+const Compiler = require('../lib/compiler.js')
+
+const compiler = new Compiler(config)
+compiler.run()
